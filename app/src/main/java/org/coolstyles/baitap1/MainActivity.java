3eb6a315ca6218c98227edc0ee12c8b514029d9a
Thanh Tung <thanhtung06112004@gmail.com>
2025-12-09 14:14:19 +0700

    Finish Activity Lifecycle implementation and logging

--- app/src/main/java/org/coolstyles/baitap1/MainActivity.java
+++ app/src/main/java/org/coolstyles/baitap1/MainActivity.java
@@ -1,24 +1,58 @@
-package org.coolstyles.baitap1;
+package org.coolstyles.baitap1; // Tên package của bạn, giữ nguyên
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.util.Log; // Cần import thư viện Log để sử dụng Log.d
+
+// KHÔNG CẦN các thư viện EdgeToEdge, Insets, ViewCompat, WindowInsetsCompat cho bài tập này.
+// Có thể xóa các import đó nếu bạn không dùng chúng trong body code.
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Lifecycle"; // Dùng tag này để lọc Logcat
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // Thay vì dùng EdgeToEdge, chỉ cần gọi setContentView nếu layout của bạn là R.layout.activity_main
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Log.d(TAG, "Hàm onCreate được gọi");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Hàm onStart được gọi");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Hàm onResume được gọi");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Hàm onPause được gọi");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Hàm onStop được gọi");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Hàm onDestroy được gọi");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "Hàm onRestart được gọi");
     }
 }
